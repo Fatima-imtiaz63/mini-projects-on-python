4ed083b93d68bbe8f1967ba7c66ae7687bf8dfd6
Fatima <fimtaiz63@gmail.com>
2025-10-17 17:14:26 +0100

    initial commit

---
+++ 2d array.py
@@ -0,0 +1,11 @@
+import numpy as np
+A = np.array([[1, 2, 3],
+              [4, 5, 5]])
+B = np.array([[6, 7, 9],
+               [3, 4, 5]])
+print("Matrix A:\n", A)
+print("matrix B:\n", B)
+print("\n A+B =\n, A + B")
+print("\n A-B =\n, A - B" )
+print("\n transpose of A:\n", A.T)
+print("\n transpose of B:\n", B.T)
